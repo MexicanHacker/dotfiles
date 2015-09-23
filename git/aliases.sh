d966c7288101efb7dec53d7ecbf662ee1851acdc
Oscar Gonzalez <oscar@sawyereffect.com>
2015-09-23 11:38:49 -0400

    Adding git aliases and vimrc

---
+++ git/aliases.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+git config --global alias.st 'status -sb'
+git config --global alias.d diff
+git config --global alias.cm commit
+git config --global alias.co checkout
+git config --global alias.b 'branch'
--- masiosare.sh
+++ masiosare.sh
@@ -1,1 +1,2 @@
+#!/bin/bash 
 ln -sv ~/projects/git/dotfiles/system/.aliases ~
